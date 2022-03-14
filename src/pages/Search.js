372563be1f9282cb68db2eb7ba7d78d2bce6b86e
hw1te <gutxander@gmail.com>
2022-03-14 18:53:34 -0300

    Requisito 5 passando.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,40 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      disabled: true,
+      artist: '',
+    };
+  }
+
+  handleChange = ({ target: { value } }) => {
+    const nameLenght = value.length;
+    const minLength = 2;
+    this.setState({ artist: value, disabled: nameLenght < minLength });
+  }
+
   render() {
+    const { disabled, artist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          placeholder="Nome do artista."
+          value={ artist }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ disabled }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
