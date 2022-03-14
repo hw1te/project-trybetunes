04e5aa9bd451f0394a6a79c7188cd4daead5d173
hw1te <gutxander@gmail.com>
2022-03-14 17:41:41 -0300

    Ate o req 4 passando.

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ class App extends Component {
             <Route path="/search" component={ Search } />
             <Route path="/album/:id" component={ Album } />
             <Route path="/favorites" component={ Favorites } />
-            <Route path="/profile" component={ Profile } />
             <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route path="/profile" component={ Profile } />
             <Route path="*" component={ NotFound } />
           </Switch>
         </BrowserRouter>
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,52 +1,15 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Header from '../components/Header';
-import getMusics from '../services/musicsAPI';
-import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
-  constructor() {
-    super();
-
-    this.setState = {
-      songs: [],
-      artist: '',
-      albumName: '',
-    };
-  }
-
-  componentDidMount() {
-    this.getMusicApi();
-  }
-
-  getMusicApi = async () => {
-    const { id } = this.props;
-    const data = await getMusics(id);
-    console.log(data);
-    this.setState({
-      songs: data,
-      artist: data.artistName[0],
-      albumName: data.collectionName[0],
-    });
-  }
-
   render() {
-    const { artist, albumName, songs } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        <span data-testid="album-name">{ albumName }</span>
-        <p data-testid="artist-name">{ artist }</p>
-        { songs.map((song) => (
-          <MusicCard key={ song } song={ song } />
-        ))}
+        <p>Album</p>
       </div>
     );
   }
 }
-
-Album.propTypes = {
-  id: PropTypes.string.isRequired,
-};
 
 export default Album;
